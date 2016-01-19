a78ec42dc0df48756be250b3fc96f90dd0b57622
boyanpeychev <boyan@peychev.net>
2016-01-19 12:47:54 +0200

    hourly stats reporting for the last month

---
+++ dns-stats-reports/README.md
@@ -0,0 +1,52 @@
+# last-month-hourly-stats.php
+With this script you can send an e-mail report with a hourly DNS statistics for the last month for a list with DNS zones.
+
+# Configuration
+**AUTH_ID** should be your API user ID
+**AUTH_PASS** should be the password of your API user
+
+In the **$dns_zones** variable you have to add a list with DNS zone names for which you want to get reports.
+
+In the **$mail_to** variable you have to add a list with e-mail addresses where you want reports to be send.
+
+In the **$mail_from**  variable you have to add an e-mail address from which the reports will be send.
+
+```php
+<?php
+
+// list with the DNS zones you want to get reports
+$dns_zones = array(
+	'example.com',
+	'example.net',
+);
+
+// list with the e-mails where will be send the reports
+$mail_to = array(
+	'mail@example.net',
+	'mail@example.com',
+);
+
+// e-mail which will be used for the sending of the reports
+$mail_from = 'report@example.net';
+```
+
+# Usage
+When you are ready with your configuration, you can run the script with following command:
+```
+php last-month-hourly-stats.php
+```
+
+# Output
+The script will output a list with the domain names for which the reports are generated and sent.
+
+# Possible problems
+If your API user ID or Password are incorrect, you will get an error message.
+
+If you are entered an invalid DNS zones in the list, you will get an error message.
+
+# Cron job
+You can add this script to be executed each first day from the month.
+Example:
+```
+0 0 1 * * /usr/bin/php /path/to/last-month-hourly-stats.php >/dev/null
+```
---
+++ dns-stats-reports/last-month-hourly-stats.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Settings
+ */
+
+// Auth ID and Password
+define("AUTH_ID", 0);
+define("AUTH_PASS", "xxx");
+
+// list with the DNS zones you want to get reports
+$dns_zones = array(
+	'example.com',
+	'example.net',
+);
+
+// list with the e-mails where will be send the reports
+$mail_to = array(
+	'mail@example.net',
+	'mail@example.com',
+);
+
+// e-mail which will be used for the sending of the reports
+$mail_from = 'report@example.net';
+
+
+/**
+ * Code
+ */
+
+// checking if we can log in successfully
+$login = apiCall('dns/login.json', "");
+if (isset($login['status']) && $login['status'] == 'Failed') {
+	die($login['statusDescription']);
+}
+
+$last_month = strtotime('-1 month');
+
+foreach ($dns_zones as $zone) {
+	$xls = '<?xml version="1.0"?><?mso-application progid="Excel.Sheet"?><Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">';
+	for ($i=date('Y-m-d', $last_month); $i<=date('Y-m-d'); $i=date('Y-m-d', strtotime('+1 day', strtotime($i)))) {
+		$time = strtotime($i);
+		$xls .= '<Worksheet ss:Name="'. date("M d, Y", $time).'"><Table>';
+		$year = date('Y', $time);
+		$month = date('m', $time);
+		$day = date('d', $time);
+		$respone = apiCall('dns/statistics-hourly.json', "domain-name={$zone}&year={$year}&month={$month}&day={$day}");
+		if (isset($response['status']) && $response['status'] == 'Failed') {
+			echo "Statistics for {$zone} failed: {$response['statusDescription']}";
+			continue 2;
+		}
+
+		foreach ($respone as $day => $requests) {
+			$xls .= "<Row><Cell><Data ss:Type=\"String\">{$day}</Data></Cell><Cell><Data ss:Type=\"Number\">{$requests}</Data></Cell></Row>";
+		}
+		$xls .= '</Table></Worksheet>';
+	}
+	$xls .= '</Workbook>';
+	
+	$subject = "{$zone} DNS stats for the last month";
+	$message = "Hello,\n\nAs attachment are added DNS statistics for zone {$zone} for the last month.\n\nRegards,\nClouDNS";
+
+	$mail = implode(', ', $mail_to);
+	if (!mail_stats($mail, $mail_from, $subject, $message, $xls, date('Y-m-d')."-{$zone}.xls")) {
+		echo "The report for {$zone} cannot be send to {$mail}\n";
+	} else {
+		echo "The report for {$zone} has been send to {$mail}\n";
+	}
+}
+
+// function to connect to the API
+function apiCall ($url, $data) {
+	$url = "https://api.cloudns.net/{$url}";
+	$data = "auth-id=".AUTH_ID."&auth-password=".AUTH_PASS."&{$data}";
+
+	$init = curl_init();
+	curl_setopt($init, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($init, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($init, CURLOPT_URL, $url);
+	curl_setopt($init, CURLOPT_POST, true);
+	curl_setopt($init, CURLOPT_POSTFIELDS, $data);
+
+	$content = curl_exec($init);
+
+	curl_close($init);
+
+	return json_decode($content, true);
+}
+
+// function for sending of stats to the email
+function mail_stats($mail_to, $from_mail, $subject, $message, $attachment_content, $attachment_filename) {
+	$content = chunk_split(base64_encode($attachment_content));
+	
+	$uid = md5(uniqid(microtime()));
+	
+	$header = "From: ".$from_mail."\r\n";
+	$header .= "MIME-Version: 1.0\r\n";
+	$header .= "Content-Type: multipart/mixed;\r\n boundary=\"------------".$uid."\"\r\n";
+	$header .= "This is a multi-part message in MIME format.\r\n";
+	
+	$mail = "--------------".$uid."\r\n";
+	$mail .= "Content-type: text/plain; charset=utf-8\r\n";
+	$mail .= "Content-Transfer-Encoding: 8bit\r\n\r\n";
+	$mail .= $message."\r\n";
+
+	
+	$mail .= "--------------{$uid}\r\n";
+	$mail .= "Content-Type: application/octet-stream; name=\"".$attachment_filename."\"\r\n";
+	$mail .= "Content-Transfer-Encoding: base64\r\n";
+	$mail .= "Content-Disposition: attachment; filename=\"".$attachment_filename."\"\r\n\r\n";
+	$mail .= $content."\r\n";
+	$mail .= "--------------{$uid}--\r\n";
+
+	if (mail($mail_to, $subject, $mail, $header)) {
+		return true;
+	}
+	return false;
+}
