e32ac118bc59ef4eb0176c1c1cc1dc2d502b6063
Boyan Peychev <boyan@peychev.net>
2018-02-09 12:29:16 +0200

    buk soa update script dns zone files export script

---
+++ bulk-soa-update/bulk-soa-update.php
@@ -0,0 +1,64 @@
+<?php
+
+// Auth ID and Password
+define("AUTH_ID", 70);
+define("AUTH_PASS", "lanex24api26");
+
+// SOA record to be set to all zones
+$soa = array(
+	'primary-ns' => 'pns1.cloudns.net',
+	'admin-mail' => 'support@cloudns.net',
+	'refresh' => 7200,
+	'retry' => 1800,
+	'expire' => 1209600,
+	'default-ttl' => 3600,
+);
+
+/*
+ * The configuration end here
+ * 
+ * Do not change anything below this lines, 
+ * if you do not know what you are doing
+ */
+
+// checking if we can log in successfully
+$login = apiCall('dns/login.json', "");
+if (isset($login['status']) && $login['status'] == 'Failed') {
+	die($login['statusDescription']);
+}
+
+// getting all the domains
+$rows_per_page = 100; // 100 is the maximum
+$z = $y = 0;
+$pages = apiCall('dns/get-pages-count.json', "rows-per-page={$rows_per_page}");
+for ($i=1; $i<=$pages; $i++) {
+	foreach (apiCall('dns/list-zones.json', "page={$i}&rows-per-page={$rows_per_page}") as $page => $zone) {
+		if ($zone['type'] != 'master') {
+			continue;
+		}
+		$z++; // counts the zones that can be updated
+		$response = apiCall('dns/modify-soa.json', "domain-name={$zone['name']}&primary-ns={$soa['primary-ns']}&admin-mail={$soa['admin-mail']}&refresh={$soa['refresh']}&retry={$soa['retry']}&expire={$soa['expire']}&default-ttl={$soa['default-ttl']}");
+		if (isset($response['status']) && $response['status'] == 'Failed') {
+			echo $zone['name']." was not updated\n";
+		} else {
+			echo $zone['name']." was updated successfully\n";
+			$y++; // counts the zones that are updated
+		}
+	}
+}
+
+echo "Updated zones: {$y} of {$z} master zones\n";
+
+function apiCall ($url, $data) {
+	$url = "http://api.cloudns.net/{$url}";
+	$data = "auth-id=".AUTH_ID."&auth-password=".AUTH_PASS."&{$data}";
+	$init = curl_init();
+	curl_setopt($init, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($init, CURLOPT_URL, $url);
+	curl_setopt($init, CURLOPT_POST, true);
+	curl_setopt($init, CURLOPT_POSTFIELDS, $data);
+	$content = curl_exec($init);
+	curl_close($init);
+	return json_decode($content, true);
+}
+
---
+++ dns-zone-files-export/dns-zone-files-export.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Settings
+ */
+
+// Auth ID and Password
+define("AUTH_ID", 0);
+define("AUTH_PASS", "xxx");
+// the directory where the zone files will be stored
+define("ZONES_DIR", "/path/to/directory");
+
+/**
+ * Code
+ */
+
+// checking if we can log in successfully
+$login = apiCall('dns/login.json', "");
+if (isset($login['status']) && $login['status'] == 'Failed') {
+	die($login['statusDescription']."\n");
+}
+// if the zone directory doesn't exist it will be created with full permissions
+if (!is_dir(ZONES_DIR)) {
+	mkdir(ZONES_DIR, 0777, true);         
+}
+
+$totalZones = $exportedZones = 0;
+$rows_per_page = 100; // 100 is the maximum
+$pages = apiCall('dns/get-pages-count.json', "rows-per-page={$rows_per_page}");
+for ($i=1; $i<=$pages+1; $i++) {
+	foreach (apiCall('dns/list-zones.json', "page={$i}&rows-per-page={$rows_per_page}") as $zone) {
+		if ($zone['type'] != 'master' || !in_array($zone['zone'], array('domain', 'ipv4', 'ipv6'))) {
+			continue;
+		}
+		
+		$totalZones++;
+		echo "Exporting {$zone['name']}... \n";
+		
+		$response = apiCall('dns/records-export.json', "domain-name={$zone['name']}");
+		if (isset($response['status']) && $response['status'] == 'Failed') {
+			echo " \t{$response['statusDescription']}\n";
+		} else {
+			$fileName = str_replace('/', '_', $zone['name']);
+			if (file_put_contents(ZONES_DIR.'/'.$fileName, $response['zone'])) {
+				echo " \tSuccess!\n";
+				$exportedZones++;
+			} else {
+				echo " \tFailed!\n";
+			}
+		}
+	}
+}
+echo "\nTotal zones exported: ".$exportedZones." out of ".$totalZones." master zones\n";
+
+// function to connect to the API
+function apiCall ($url, $data) {
+	$url = "https://api.cloudns.net/{$url}";
+	$data = "auth-id=".AUTH_ID."&auth-password=".AUTH_PASS."&{$data}";
+
+	$init = curl_init();
+	curl_setopt($init, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($init, CURLOPT_URL, $url);
+	curl_setopt($init, CURLOPT_POST, true);
+	curl_setopt($init, CURLOPT_POSTFIELDS, $data);
+
+	$content = curl_exec($init);
+
+	curl_close($init);
+
+	return json_decode($content, true);
+}
