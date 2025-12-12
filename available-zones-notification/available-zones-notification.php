20576d0e1f3bcebf31a774972ad77c7203536b6f
Yordan Minkov <60508521+yminkov@users.noreply.github.com>
2025-12-12 11:49:16 +0200

    Update available-zones-notification.php

--- available-zones-notification/available-zones-notification.php
+++ available-zones-notification/available-zones-notification.php
@@ -19,11 +19,12 @@ function apiCall ($url, $data) {
 	curl_setopt($init, CURLOPT_URL, $url);
 	curl_setopt($init, CURLOPT_POST, true);
 	curl_setopt($init, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($init, CURLOPT_USERAGENT, 'cloudns_api_script/0.1 (+https://github.com/ClouDNS/cloudns-api-bulk-updates/tree/master/available-zones-notification)');
 
 	$content = curl_exec($init);
 
-	curl_close($init);
+	if (PHP_VERSION_ID < 80000) {
+        curl_close($init);
+    }
 
 	return json_decode($content, true);
 }
