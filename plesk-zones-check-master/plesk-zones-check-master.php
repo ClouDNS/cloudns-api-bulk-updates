59e9d13361b8641b24145cafafbb4f5d5aaf39b7
Yordan Minkov <60508521+yminkov@users.noreply.github.com>
2025-12-12 12:31:49 +0200

    Update plesk-zones-check-master.php

--- plesk-zones-check-master/plesk-zones-check-master.php
+++ plesk-zones-check-master/plesk-zones-check-master.php
@@ -32,7 +32,9 @@ function apiCall($url, $data) {
     curl_setopt($init, CURLOPT_POSTFIELDS, $data);
     curl_setopt($init, CURLOPT_USERAGENT, 'cloudns_api_script/0.1 (+https://github.com/ClouDNS/cloudns-api-bulk-updates/tree/master/plesk-zones-check-master)');
     $content = curl_exec($init);
-    curl_close($init);
+    if (PHP_VERSION_ID < 80000) {
+        curl_close($init);
+    }
     return json_decode($content, true);
 }
 
