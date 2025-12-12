57dc2caad083342ea4dac235ae5eb2b6a719fac1
Yordan Minkov <60508521+yminkov@users.noreply.github.com>
2025-12-12 12:38:04 +0200

    Update add.php

--- secondary-ips-management/add.php
+++ secondary-ips-management/add.php
@@ -22,7 +22,9 @@ function apiCall ($url, $data) {
 	curl_setopt($init, CURLOPT_POSTFIELDS, $data);
 	curl_setopt($init, CURLOPT_USERAGENT, 'cloudns_api_script/0.1 (+https://github.com/ClouDNS/cloudns-api-bulk-updates/tree/master/secondary-ips-management/add.php)');
 	$content = curl_exec($init);
-	curl_close($init);
+    if (PHP_VERSION_ID < 80000) {
+        curl_close($init);
+    }
 	return json_decode($content, true);
 }
 
