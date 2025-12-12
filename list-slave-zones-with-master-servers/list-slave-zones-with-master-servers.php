34d8f1d93c2f29cd058d737e5f754c618fe4fdd4
Yordan Minkov <60508521+yminkov@users.noreply.github.com>
2025-12-12 12:23:26 +0200

    Update list-slave-zones-with-master-servers.php

--- list-slave-zones-with-master-servers/list-slave-zones-with-master-servers.php
+++ list-slave-zones-with-master-servers/list-slave-zones-with-master-servers.php
@@ -14,7 +14,9 @@ function apiCall ($url, $data) {
  curl_setopt($init, CURLOPT_POSTFIELDS, $data);
  curl_setopt($init, CURLOPT_USERAGENT, 'cloudns_api_script/0.1 (+https://github.com/ClouDNS/cloudns-api-bulk-updates/tree/master/list-slave-zones-with-master-servers/list-slave-zones-with-master-servers)');
  $content = curl_exec($init);
- curl_close($init);
+ if (PHP_VERSION_ID < 80000) {
+    curl_close($init);
+ }
  return json_decode($content, true);
 }
 // checking if we can log in successfully
